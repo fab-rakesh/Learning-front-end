37d2f45379050512411c91b1d3fa08c6e32454d3
fab-rakesh <ed.rakesh.ramteke@gmail.com>
2023-01-18 12:23:49 +0530

    Add change name with localStorage

--- firstProject/scripts/main.js
+++ firstProject/scripts/main.js
@@ -17,17 +17,32 @@
 
 /**
  * Change Name
+ * 
+ *  1. On page load, Check if name is in local storage, if not, Show default name
+
+    2. if yes, show stored name
+
+    3. user can add a new name using change name button, store new name in the storage then display it
  */
 
 let myButton = document.querySelector('button'); 
 let myHeading = document.querySelector('h1');
 
-// add onclick event to button
-myButton.onclick = () => {
-    // prompt for username
-    let myUsername = prompt('Enter your name.');
+    // if there's username in localStorage, display it
+    let myLocalName = localStorage.getItem('name');
+    if (myLocalName) {
+        myHeading.textContent = "Hi, I'm " + myLocalName;
+    }
+    
 
-    // update the h1 with new username
-    if (myUsername)
-        myHeading.textContent = "Hi, I'm " + myUsername;
-}
+    // add onclick event to button
+    myButton.onclick = () => {
+        // prompt for username
+        let myUsername = prompt('Enter your name.');
+
+        // store name in localStorage & update the h1 with new username
+        if (myUsername) {
+            localStorage.setItem('name', myUsername);
+            myHeading.textContent = "Hi, I'm " + myUsername;
+        }     
+    }
